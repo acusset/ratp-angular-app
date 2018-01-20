a97ea1c4f5fc0c7252998e2bb7748cae49c01723
Antoine Cusset <acusset@clever-age.com>
2018-01-20 17:02:53 +0100

    Update package.json
    fix imports
    
    add service worker lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule, MatDividerModule, MatTabsModule} from '@angular/material';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,11 @@ import {MatCardModule, MatDividerModule, MatTabsModule} from '@angular/material'
     MatCardModule,
     MatTabsModule,
     MatDividerModule,
-    HttpClientModule
+    HttpClientModule,
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
