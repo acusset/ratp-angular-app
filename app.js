c9676afe678f8535aad3eb7f017f4c063c31b1d7
Antoine Cusset <acusset@clever-age.com>
2018-01-20 16:36:31 +0100

    add express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (request, response) => {});
+
+const port = process.env.PORT || '3001';
+app.set(port, 'port');
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Server running !'));
