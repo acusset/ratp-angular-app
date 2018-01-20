3e11d8539e1d9f8df2d456703e3ec38da48c121d
Antoine Cusset <acusset@clever-age.com>
2018-01-20 18:00:46 +0100

    Add gitlab-ci.yml

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ const port = process.env.PORT || '3001';
 app.set(port, 'port');
 
 const server = http.createServer(app);
-server.listen(port, () => console.log('Server running !'));
+server.listen(port);
